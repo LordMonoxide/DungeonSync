9a7b631e435f5d6fb12a41ae453e8fbd4b2f5cfa
Corey Frenette <corey@narwhunderful.com>
2014-01-08 02:37:49 -0400

    Refactor

--- client/src/dungeonsync/DungeonSync.java
+++ client/src/dungeonsync/DungeonSync.java
@@ -1,13 +1,109 @@
 package dungeonsync;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
 import javax.swing.JFrame;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 public class DungeonSync {
+  private static final String baseURL = "http://dungeonsync.monoxidedesign.com/";
+
   JFrame _frame;
   
   public DungeonSync() {
     _frame = new JFrame("Dungeon Sync");
     _frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     _frame.setVisible(true);
+  }
+  
+  public void request(String url, String method, Parameter... param) {
+    request(url, method, "UTF-8", param);
+  }
+  
+  public void request(String url, String method, String encoding, Parameter... param) {
+    HttpURLConnection con = null;
+    try {
+      con = (HttpURLConnection)new URL(baseURL + url).openConnection();
+      con.setRequestMethod("PUT");
+      con.setRequestProperty("Accept-Charset", encoding);
+      con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=" + encoding);
+      con.setUseCaches(false);
+      con.setDoInput(true);
+      con.setDoOutput(true);
+      
+      String params = "";
+      for(Parameter p : param) {
+        if(params.length() != 0) { params += "&"; }
+        params += p.key + "=" + URLEncoder.encode(p.val, encoding);
+      }
+      
+      OutputStream out = con.getOutputStream();
+      out.write(params.getBytes("UTF-8"));
+      out.close();
+      
+      JSONObject json = JSONFromInputStream(con.getInputStream());
+      System.out.println(json);
+    } catch(MalformedURLException e) {
+      e.printStackTrace();
+    } catch(IOException e) {
+      e.printStackTrace();
+      
+      if(con != null) {
+        JSONObject json = JSONFromInputStream(con.getErrorStream());
+        for(Object err : json.keySet()) {
+          String error = (String)err;
+          System.out.println(error + ":");
+          
+          for(int i = 0; i < json.getJSONArray(error).length(); i++) {
+            System.out.println(json.getJSONArray(error).get(i));
+          }
+        }
+      }
+    }
+  }
+  
+  public Parameter param(String key, String val) {
+    return new Parameter(key, val);
+  }
+  
+  private JSONObject JSONFromInputStream(InputStream is) {
+    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+    String line;
+    StringBuffer resp = new StringBuffer();
+    
+    try {
+      while((line = rd.readLine()) != null) {
+        line = line.substring(1, line.length() - 1);
+        line = line.replaceAll("\\\\\"", "\"");
+        resp.append(line);
+        resp.append('\r');
+      }
+    } catch(IOException e1) {
+      e1.printStackTrace();
+    }
+    
+    try {
+      rd.close();
+    } catch(IOException e1) { }
+    
+    return new JSONObject(new JSONTokener(resp.toString()));
+  }
+  
+  public static class Parameter {
+    public final String key, val;
+    public Parameter(String key, String val) {
+      this.key = key;
+      this.val = val;
+    }
   }
 }
--- client/src/main/Main.java
+++ client/src/main/Main.java
@@ -1,114 +1,12 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-
-import javax.swing.SwingUtilities;
-
-import org.json.HTTP;
-import org.json.HTTPTokener;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
 import dungeonsync.DungeonSync;
 
 public class Main {
-  public static void main(String[] args) throws UnsupportedEncodingException {
+  public static void main(String[] args) {
     System.out.println("Starting...");
     
-    /*SwingUtilities.invokeLater(new Runnable() {
-      @Override
-      public void run() {
-        new DungeonSync();
-      }
-    });*/
-    
-    String params = "email="                 + URLEncoder.encode("corey@narwhunderful.com", "UTF-8") +
-                   "&password="              + URLEncoder.encode("monoxide", "UTF-8") +
-                   "&password_confirmation=" + URLEncoder.encode("monoxide", "UTF-8");
-    
-    HttpURLConnection con = null;
-    
-    URL url;
-    
-    try {
-      url = new URL("http://dungeonsync.monoxidedesign.com/api/register");
-    } catch(MalformedURLException e) {
-      e.printStackTrace();
-      return;
-    }
-    
-    try {
-      con = (HttpURLConnection)url.openConnection();
-      con.setRequestMethod("PUT");
-      con.setRequestProperty("Accept-Charset", "UTF-8");
-      con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-      con.setUseCaches(false);
-      con.setDoInput(true);
-      con.setDoOutput(true);
-      
-      OutputStream out = con.getOutputStream();
-      out.write(params.getBytes("UTF-8"));
-      out.close();
-      
-      InputStream is = con.getInputStream();
-      BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-      String line;
-      StringBuffer resp = new StringBuffer();
-      
-      while((line = rd.readLine()) != null) {
-        line = line.substring(1, line.length() - 1);
-        line = line.replaceAll("\\\\\"", "\"");
-        resp.append(line);
-        resp.append('\r');
-      }
-      
-      rd.close();
-      
-      JSONObject json = new JSONObject(new JSONTokener(con.getInputStream()));
-      System.out.println(json);
-    } catch(IOException e) {
-      e.printStackTrace();
-      
-      if(con != null) {
-        BufferedReader rd = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-        String line;
-        StringBuffer resp = new StringBuffer();
-        
-        try {
-          while((line = rd.readLine()) != null) {
-            line = line.substring(1, line.length() - 1);
-            line = line.replaceAll("\\\\\"", "\"");
-            resp.append(line);
-            resp.append('\r');
-          }
-        } catch(IOException e1) {
-          e1.printStackTrace();
-        }
-        
-        try {
-          rd.close();
-        } catch(IOException e1) { }
-        
-        JSONObject json = new JSONObject(new JSONTokener(resp.toString()));
-        for(Object err : json.keySet()) {
-          String error = (String)err;
-          System.out.println(error + ":");
-          
-          for(int i = 0; i < json.getJSONArray(error).length(); i++) {
-            System.out.println(json.getJSONArray(error).get(i));
-          }
-        }
-      }
-    }
+    DungeonSync ds = new DungeonSync();
+    ds.request("api/register/", "PUT", ds.param("email", "corey2@narwhunderful.com"), ds.param("password", "monoxide"), ds.param("password_confirmation", "monoxide"));
   }
 }
