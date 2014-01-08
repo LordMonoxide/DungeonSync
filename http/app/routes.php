54c0828648aaa88cf640b8f94fefcc1195463e42
Corey Frenette <corey@narwhunderful.com>
2014-01-08 01:47:18 -0400

    Add routes

--- http/app/routes.php
+++ http/app/routes.php
@@ -1,17 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
-Route::get('/', function()
-{
-	return View::make('hello');
+Route::group(['prefix' => 'api'], function() {
+  Route::put('/register', ['as' => 'api.register', 'uses' => 'api\AuthController@register']);
+  Route::put('/login',    ['as' => 'api.login',    'uses' => 'api\AuthController@login']);
+  Route::put('/logout',   ['as' => 'api.logout',   'uses' => 'api\AuthController@logout']);
 });
