0bc38968d30ab8bf11634ae2a9e6571ad721d290
Corey Frenette <corey@narwhunderful.com>
2014-01-10 16:07:13 -0400

    Update lang

--- http/app/lang/en/app.php
+++ http/app/lang/en/app.php
@@ -4,11 +4,12 @@ return [
   'title' => 'Dungeon Sync',
   'email' => 'Email',
   'password' => 'Password',
-  'password_confirmation' => 'Confirm Password',
+  'password_confirmation' => 'Confirm',
   'rememberme' => 'Remember Me',
   'register' => 'Register',
-  'login' => 'Log in',
-  'logout' => 'Log out',
+  'login' => 'Log In',
+  'logout' => 'Log Out',
+  'newaccount' => 'New Account',
   'orregister' => 'or ' . HTML::linkRoute('home.register', 'register'),
   'orlogin' => 'or ' . HTML::linkRoute('home.login', 'login')
 ];
