2bd0da6f3e0856f1f6ddafe8f8eafdb79784fe95
Corey Frenette <corey@narwhunderful.com>
2014-01-10 02:36:17 -0400

    Add routes to char API

--- http/app/routes.php
+++ http/app/routes.php
@@ -4,6 +4,13 @@ Route::group(['prefix' => 'api'], function() {
   Route::put('/register', ['as' => 'api.auth.register', 'uses' => 'api\AuthController@register']);
   Route::put('/login',    ['as' => 'api.auth.login',    'uses' => 'api\AuthController@login']);
   Route::put('/logout',   ['as' => 'api.auth.logout',   'uses' => 'api\AuthController@logout']);
+  
+  Route::group(['prefix' => 'characters'], function() {
+    Route::model('character', 'Character');
+    
+    Route::get('/',                     ['as' => 'api.auth.characters.all',      'uses' => 'api\CharacterController@all']);
+    Route::get('/{character}/download', ['as' => 'api.auth.characters.download', 'uses' => 'api\CharacterController@download']);
+  });
 });
 
 Route::get('/',         ['as' => 'home.home',     'uses' => 'HomeController@home']);
