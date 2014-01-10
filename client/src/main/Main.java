d915bcc724fb621c952ef15fab87ed6a1bb03d82
Corey Frenette <corey@narwhunderful.com>
2014-01-10 15:44:52 -0400

    Set look and feel to system default

--- client/src/main/Main.java
+++ client/src/main/Main.java
@@ -1,12 +1,19 @@
 package main;
 
+import javax.swing.UIManager;
+
 import dungeonsync.DungeonSync;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("Starting...");
     
-    DungeonSync ds = new DungeonSync();
-    ds.login("corey@narwhunderful.com", "monoxide");
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+    
+    new DungeonSync();
   }
 }
