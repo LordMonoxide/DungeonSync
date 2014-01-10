d98ab851c2058a2a76b0b8611f295d566cba43e6
Corey Frenette <corey@narwhunderful.com>
2014-01-09 23:53:44 -0400

    Functional file uploads

--- http/app/controllers/CharacterController.php
+++ http/app/controllers/CharacterController.php
@@ -15,17 +15,33 @@ class CharacterController extends BaseController {
   }
   
   public function store() {
-    $validator = Validator::make(Input::all(), [
-      'character' => ['required']
+    if(Input::file('character') === NULL) {
+      die('No char');
+    }
+    
+    $file = Input::file('character');
+    $name = substr($file->getClientOriginalName(), -$file->getClientOriginalExtension());
+    $validator = Validator::make([
+      'name' => $name,
+      'ext' => $file->getClientOriginalExtension()
+    ], [
+      'name' => ['required', 'unique:characters,original,NULL,id,user_id,' . Auth::user()->id],
+      'ext' => ['in:dsparty']
     ]);
     
     if($validator->passes()) {
-      $file = Input::file('character');
       echo $file->getRealPath() . '<br>';
       echo $file->getClientOriginalName() . '<br>';
       echo $file->getClientOriginalExtension() . '<br>';
       echo $file->getSize() . '<br>';
-      $file->move(storage_path() . '\uploads', str_random(64));
+      
+      $character = new Character;
+      $character->user()->associate(Auth::user());
+      $character->filename = str_random(64);
+      $character->original = $name;
+      $character->save();
+      
+      $file->move(storage_path() . '\uploads', $character->filename);
     } else {
       echo $validator->messages();
     }
