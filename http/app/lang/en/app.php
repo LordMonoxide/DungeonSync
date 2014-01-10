916d6fe936707857aba6cab40885557fcf8e0c16
Corey Frenette <corey@narwhunderful.com>
2014-01-10 00:13:08 -0400

    Update lang

--- http/app/lang/en/app.php
+++ http/app/lang/en/app.php
@@ -8,6 +8,7 @@ return [
   'rememberme' => 'Remember Me',
   'register' => 'Register',
   'login' => 'Log in',
+  'logout' => 'Log out',
   'orregister' => 'or ' . HTML::linkRoute('home.register', 'register'),
   'orlogin' => 'or ' . HTML::linkRoute('home.login', 'login')
 ];
---
+++ http/app/lang/en/characters.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+  'list' => 'Character list',
+  'add' => 'Upload character',
+  'filename' => 'Filename',
+  'created' => 'Created',
+  'updated' => 'Updated',
+  'upload' => 'Upload'
+];
