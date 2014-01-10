4f69841e9780da050c62b0173b55c38a91e74b82
Corey Frenette <corey@narwhunderful.com>
2014-01-10 02:35:53 -0400

    API for getting characters

---
+++ http/app/controllers/api/CharacterController.php
@@ -0,0 +1,19 @@
+<?php namespace api;
+
+use Auth;
+use Controller;
+use Response;
+
+class CharacterController extends Controller {
+  public function __construct() {
+    $this->beforeFilter('auth.401');
+  }
+  
+  public function all() {
+    return Response::json(Auth::user()->characters->toJson(), 200);
+  }
+  
+  public function download($character) {
+    return Response::download(storage_path() . '/uploads/' . $character->filename, $character->original, ['Content-Type' => 'application/octet-stream']);
+  }
+}
